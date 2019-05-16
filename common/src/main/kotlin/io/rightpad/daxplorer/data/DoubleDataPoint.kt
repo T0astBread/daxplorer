2f5a3bc2b8dd06089bb8ea95883b6fb589e62e8b
T0astBread <tb@t0ast.cc>
2019-05-16 20:19:47 +0200

    Cleaned up RSI code

--- common/src/main/kotlin/io/rightpad/daxplorer/data/DoubleDataPoint.kt
+++ common/src/main/kotlin/io/rightpad/daxplorer/data/DoubleDataPoint.kt
@@ -3,6 +3,4 @@ package io.rightpad.daxplorer.data
 import io.rightpad.daxplorer.data.datapoints.absolute.TimeSeriesDataPoint
 import java.time.LocalDateTime
 
-class DoubleDataPoint( val featureValue:Double, timestamp: LocalDateTime): TimeSeriesDataPoint(timestamp) {
-
-}
+class DoubleDataPoint(val featureValue:Double, timestamp: LocalDateTime): TimeSeriesDataPoint(timestamp)
